2d3018477799e6da99344758c9ff1d629fefb55c
Kishukumar07 <kishukumars082@gmail.com>
2025-06-04 19:02:26 +0530

    array ,string ,tuples in ts

---
+++ 05.INTRO-Typescript/index.ts
@@ -0,0 +1,109 @@
+//Declaring a Variable 
+let a: number = 10;
+let b: String = "Masai";
+let c: boolean = false;
+// c = 10 ; //you can not reassing the c with value where value != declared datatype. 
+
+
+//Arrays ...
+let d: number[] = [1, 2, 3, 4]; //array declaration and initilization ...
+
+
+
+//Strings ...
+let e: string[] = ["1", "2"];
+
+//Tuples(Not part of JS)   
+//RGB(100,200,10) 
+let RGB :[number, number,number]=[100,200,10]; //tuples -> limiting the number of elements that array can have; 
+
+//if we want n=80  in that case array should be best choice;   
+//tuples is only for possible count of element . eg 3/4/5 elements Only.... 
+
+let code: [number , number ,string ] =[100,200,"10"] //diff type of element is allowed in   
+
+
+
+
+// any thing that can be done in js can not be done in js that is also the limitations of the ts ... ...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
