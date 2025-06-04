9c6fcfb378f6433f2a64a27cf1f8c3dfa5c0f461
Kishukumar07 <kishukumars082@gmail.com>
2025-06-04 20:40:31 +0530

    required true in ts by default in typevar

---
+++ 05.INTRO-Typescript/index1.ts
@@ -0,0 +1,77 @@
+//we are going to define a user type
+
+type User ={
+    name : string ; 
+    age : number ; 
+    email: string ;
+    phone : number;
+    password : string ; 
+} ; 
+
+//BY DEFAULT IN CASE OF TYPE SCRIPT EVERY KEY THAT WE DEFINE  IN TYPE VARIABLE IS GOING TO BE  "REQUIRED"  IS GOING TO BE IMPORTANT .... 
+const u1 : User ={
+name: "niket",
+age : 22,
+email:"niket@gmail.com",
+phone : 43434,                //comment any field and see required error 
+password : "232332"
+}
+
+// if dont have to put any  value (eg remove phone no wla field) ...
+
+//we are getting big red error s that means every field we defined in type user is required just like required:true in mongodb... 
+
+//BY DEFAULT IN CASE OF TYPE SCRIPT EVERY KEY THAT WE DEFINE  IN TYPE VARIABLE IS GOING TO BE  "REQUIRED"  IS GOING TO BE IMPORTANT .... 
+//in case of mongo db  required is false by defaule but here it is true ...
+
+
+
+
+// lets make any key optional  ...( just put a que mark befor the type in type variable)
+
+
+
+type actor ={
+    name : string ; 
+    age : number ; 
+    email: string ;
+    phone?: number;          //see ? telling that requied is false  
+    password : string ; 
+} ; 
+
+//BY DEFAULT IN CASE OF TYPE SCRIPT EVERY KEY THAT WE DEFINE  IN TYPE VARIABLE IS GOING TO BE  "REQUIRED"  IS GOING TO BE IMPORTANT .... 
+const a1 : actor ={
+name: "niket",
+age : 22,
+email:"niket@gmail.com",
+// phone : 43434,          //comment any field and u'll not see required error
+password : "232332"
+}
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
