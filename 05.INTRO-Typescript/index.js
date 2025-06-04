4f809f26315a017b30e9d9f87cbb55a8f71b9ec0
Kishukumar07 <kishukumars082@gmail.com>
2025-06-04 20:13:48 +0530

    typescript type

--- 05.INTRO-Typescript/index.js
+++ 05.INTRO-Typescript/index.js
@@ -1,4 +1,8 @@
 "use strict";
+//IMPORTANCE OF TYPESCRIPT
+// js doesnt have any type error...
+// ts has type error  ... 
+// TS IS NOT A PROGRAMMING LANGUAGE IT IS BUILT ON JS TO OVERCOME SOME ISSUES AND TO ADD SOME FEATURES ON IT (world popular language )
 //Declaring a Variable 
 let a = 10;
 let b = "Masai";
@@ -8,7 +12,7 @@ let c = false;
 let d = [1, 2, 3, 4]; //array declaration and initilization ...
 //Strings ...
 let e = ["1", "2"];
-//Tuples(Not part of JS)
+//Tuples(Not part of JS).....
 //RGB(100,200,10) 
 let RGB = [100, 200, 10]; //tuples -> limiting the number of elements that array can have; 
 //if we want n=80  in that case array should be best choice;   
@@ -17,7 +21,7 @@ let code = [100, 200, "10"]; //diff type of element is allowed in
 // any thing that can be done in js can not be done in js that is also the limitations of the ts ... ...
 // seneario if you have to build a app for windows(microsoft) you have to use c#(built by microsoft) and  TS which is also built by microsoft thats why 
 // a lot of syntex of ts is comming from c sharp . 
-//enum 
+//enum (ts newly introduce that things)....
 var Roles;
 (function (Roles) {
     Roles[Roles["super_admin"] = 0] = "super_admin";
@@ -25,3 +29,41 @@ var Roles;
     Roles[Roles["user"] = 2] = "user";
 })(Roles || (Roles = {}));
 console.log(Roles.admin);
+// what is the usecase of this 
+// enum is used as object can used for defining or storing different roles with values but there is one usecase
+// like object we can not change the values
+//whenever we want to define some sort of object that we dont want ever to change ...
+//can also define names for all fields.. 
+var Roles1;
+(function (Roles1) {
+    Roles1["super_admin"] = "Super Admin";
+    Roles1["admin"] = "Admin";
+    Roles1["user"] = "User";
+    Roles1["sales"] = "Sales";
+})(Roles1 || (Roles1 = {}));
+//we never want to change the values .... if we want then we have to use object ..... 
+console.log(Roles1.admin);
+//we can't reassign the roles like object 
+// Roles1.admin= "niket" ; this will be a error (not allowed); 
+/*
+
+const obj ={};
+obj.name ="kdf"   ~modification
+this is allowed in js...Y
+but this is not allowed
+jjj={
+age =3 ,           ~ re assignment
+}
+
+in enum both modification and re assignment are not allowed.
+
+*/
+// #getting rid of repeatation of object ...
+console.log("Good morning Nrupul");
+console.log("Good morning Yogesh");
+console.log("Good morning prateek");
+//we are repeating some code ... if i dont want to repeat this code 
+//we can do this in function 
+const greet = "Good Morning";
+// whenever we want to use a value for multiple time we store that value in a variable and use that variable multiple Time.
+console.log(greet, "Nrupul");
--- 05.INTRO-Typescript/index.ts
+++ 05.INTRO-Typescript/index.ts
@@ -1,3 +1,10 @@
+//IMPORTANCE OF TYPESCRIPT
+// js doesnt have any type error...
+// ts has type error  ... 
+
+// TS IS NOT A PROGRAMMING LANGUAGE IT IS BUILT ON JS TO OVERCOME SOME ISSUES AND TO ADD SOME FEATURES ON IT (world popular language )
+
+
 //Declaring a Variable 
 let a: number = 10;
 let b: String = "Masai";
@@ -13,66 +20,120 @@ let d: number[] = [1, 2, 3, 4]; //array declaration and initilization ...
 //Strings ...
 let e: string[] = ["1", "2"];
 
-//Tuples(Not part of JS)
 
+
+
+//Tuples(Not part of JS).....
 //RGB(100,200,10) 
 let RGB: [number, number, number] = [100, 200, 10]; //tuples -> limiting the number of elements that array can have; 
 
 //if we want n=80  in that case array should be best choice;   
 //tuples is only for possible count of element . eg 3/4/5 elements Only.... 
-
 let code: [number, number, string] = [100, 200, "10"] //diff type of element is allowed in   
+
+
 
 
 
 
 // any thing that can be done in js can not be done in js that is also the limitations of the ts ... ...
 
-
-
-
-
 // seneario if you have to build a app for windows(microsoft) you have to use c#(built by microsoft) and  TS which is also built by microsoft thats why 
 // a lot of syntex of ts is comming from c sharp . 
 
 
-//enum 
+
+
+
+
+
+//enum (ts newly introduce that things)....
 enum Roles {
     super_admin,  //value  is 0
     admin,        //values is 1
     user,         //values is 2 
 }
 
-
-
 console.log(Roles.admin);
 
+// what is the usecase of this 
+// enum is used as object can used for defining or storing different roles with values but there is one usecase
+// like object we can not change the values
+//whenever we want to define some sort of object that we dont want ever to change ...
+
+
+ //can also define names for all fields.. 
+enum Roles1 {
+    super_admin ="Super Admin",  //value  is 0
+    admin = "Admin",        //values is 1
+    user =  "User",         //values is 2 
+    sales = "Sales" ,
+}
+//we never want to change the values .... if we want then we have to use object ..... 
+console.log(Roles1.admin) ; 
+//we can't reassign the roles like object 
+// Roles1.admin= "niket" ; this will be a error (not allowed); 
+
+
+/* 
+
+const obj ={};
+obj.name ="kdf"   ~modification 
+this is allowed in js...Y
+but this is not allowed 
+jjj={
+age =3 ,           ~ re assignment 
+} 
+
+in enum both modification and re assignment are not allowed.
+
+*/
+
+
+
+
+// #getting rid of repeatation of object ...
+
+console.log("Good morning Nrupul");
+console.log("Good morning Yogesh");
+console.log("Good morning prateek");
+//we are repeating some code ... if i dont want to repeat this code 
+//we can do this in function 
+
+
+
+const greet =  "Good Morning"; 
+// whenever we want to use a value for multiple time we store that value in a variable and use that variable multiple Time.
+
+console.log(greet, "Nrupul");
 
 
 
 
 
+//defining OBJECT  is just similar like .... defining a array in ts. 
+
+let f: {                
+    name: string; 
+    age: number;        //type 
+    isMarried: boolean;
+} = {       
+    name: "Niket",
+    age: 22,           //obj 
+    isMarried: false,
+};
 
 
 
 
+// rather than copy the whole code we store and use in variable again and again ...
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+type  = {  
+    //we are trying to store typscript type inside a js variable ....is this applicable ...  
+    name: string;
+    age: number;
+    isMarried: boolean;
+}
 
 
 
