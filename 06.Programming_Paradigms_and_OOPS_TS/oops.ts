c10cdd8be5762f4eecb67abedca1f451b875b36c
Kishukumar07 <kishukumars082@gmail.com>
2025-06-05 12:52:22 +0530

    abstraction with abstact keyword in parent class

--- 06.Programming_Paradigms_and_OOPS_TS/oops.ts
+++ 06.Programming_Paradigms_and_OOPS_TS/oops.ts
@@ -1,32 +1,32 @@
 //creating a Vehicle Parent class... 
-class  Vehicle{
-     engine :string ; 
-     wheel : number ; 
-     constructor ( engine :string , 
-     wheel : number  ){
-      this.engine= engine; 
-      this.wheel = wheel; 
-     }
+abstract class Vehicle {
+    engine: string;
+    wheel: number;
+    constructor(engine: string,
+        wheel: number) {
+        this.engine = engine;
+        this.wheel = wheel;
+    }
 }
 
 
 //for now we are going with two child classes from the abstract vehicle class ...
 //this extends wala part is inheritance 
 
-class Car extends Vehicle{   
+class Car extends Vehicle {
 
-    isAc :boolean ; 
-    price :number; 
+    isAc: boolean;
+    price: number;
 
-    constructor( engine :string , 
-     wheel : number , isAc :boolean , 
-    price :number ){
-    
-         super(engine,wheel); //used for passing arguments to parent constructor class :
-         // args ->>parms of class b ---> super() -->pased to class A parent class; 
-         //SUPER METHOD IS ACCUTALLY THE CONSTRUCTOR OF THE PARENT CLASS 
-         this.isAc=isAc,
-         this.price=price 
+    constructor(engine: string,
+        wheel: number, isAc: boolean,
+        price: number) {
+
+        super(engine, wheel); //used for passing arguments to parent constructor class :
+        // args ->>parms of class b ---> super() -->pased to class A parent class; 
+        //SUPER METHOD IS ACCUTALLY THE CONSTRUCTOR OF THE PARENT CLASS 
+        this.isAc = isAc,
+            this.price = price
 
     }
 
@@ -36,10 +36,86 @@ class Car extends Vehicle{
 
 
 
-let ford = new Car("vs4",8,true,5700000) ; 
+let ford = new Car("vs4", 8, true, 5700000);
 
 console.log(ford); //Car { engine: 'vs4', wheel: 8, isAc: true, price: 5700000 }
 console.log(ford['isAc']); //true  this property extended from vehical class .
+
+
+
+
+
+//simlarly lets creat a bus class 
+
+class Bus extends Vehicle {  //extends
+    no_of_seats: number;   
+    ticket: number;
+
+    constructor(engine: string,
+        wheel: number, no_of_seats: number, ticket: number) {  //constructor 
+
+        super(engine      //super 
+            , wheel)
+     this.no_of_seats= no_of_seats,  
+     this.ticket = ticket 
+    }
+}
+
+
+ 
+//creatig instance of Bus 
+let shreeRamRath = new Bus("engine4.3" , 16, 56, 100); 
+
+console.log(shreeRamRath.ticket);  
+
+
+//can i create a instance of Vehical class itself . 
+  
+ 
+// const v = new Vehicle("xyzengine",45)  //commented 
+
+//but dont you think buying only engine and wheel is worthit 
+//dont you think we have to restrict the object creation for this vehical class ....
+//the line 75 should not be allowed !!
+
+
+//we need somehow block instance creation of parent call directly like line no 75 and it should not be block instance creation with super method inside the cons-fun of child class . like line no   25/57.... 
+
+// here comes the abstract keyword which make the parent class allow to use its instance only from child class not directly accessable ... 
+
+//real life eg : you don't need to know how the engine works to operate the vehicle, you just focus on the steering, brakes, and accelerator.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
