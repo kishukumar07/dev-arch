6ef4854d059bfc1f2c71b25182d38293cf652a66
Kishukumar07 <kishukumars082@gmail.com>
2025-06-06 20:25:24 +0530

    singleton design pattern

--- 06.Programming_Paradigms_and_OOPS_TS/solid.js
+++ 06.Programming_Paradigms_and_OOPS_TS/solid.js
@@ -59,8 +59,8 @@ class swordmen extends player {
     slash() { }
     ; //this is how we update the class 100%
 }
-//3.L ->liscov Substitution Principle
-// 
+//3.L ->liscov Substitution Principle   
+//if you can't replace the child class with the parent class you shouldnot make that inheritance  
 class Rectacngle {
     constructor(width, height) {
         this.width = width;
@@ -94,4 +94,128 @@ const sq = new Square(5);
 console.log(sq.area()); //25 
 sq.setHeight(6);
 console.log(sq.area()); //36 should be the ans
-//I -> Interface Segrigation Principe  
+//lets do the same thing with the rectangle...
+const rec = new Rectacngle(5, 5); //should line 126 and 136 are same things .....Y
+rec.setWidth(6); //after this the rectangle !=squire  
+console.log(rec.area()); // 30  
+class Player {
+    constructor(level, attack) {
+        this.level = 1;
+        this.attack = 10;
+    }
+    levelup() {
+    }
+}
+class Enemies {
+    constructor(level, attack) {
+        this.level = 1;
+        this.attack = 10;
+    }
+    followPlayer() {
+    }
+}
+// previously there was one single interface which were used for all the classes but due to it was nto dynamic ... we'll   used inheritance on interfaces and used diff  sub_interfaces accordingly    ...
+//D -> Dependenvy Inversion Principle .... 
+//HIGHER LEVEL MODULES SHOULD'T DEPEND ON LOWER LEVEL MODULES THEY SHOULD DEPEND ON ABSTRACTIONS )
+//so we'all know about http module in node js =>createserver and we can fetch data get method    ...
+//do we know about node-fetch (package) ... 
+//so lets assume we dont know the syntex of get method for getting the data  ...  we want to fetch some data in my server not in client in my server ...
+// ->first we download node fetch package ... 
+//in every phase whenever we need some data  ..we'll use node fetch ... but you'know if nodefetch has some issues like it will hack your data or something like that so we'll think not to use it ... 
+//so in such senerio we'll try to switch to http ... 
+//node fetch =>>> Http module of fetching the data 
+//Problem :  Every where we need to change the code wherever  i used the nodefetch . 
+// what is the simplest sollution of this ...
+// inside the funcion we implement the node fetch 
+//we'll import this particular fun everywhere i need to use . 
+//NOW WE HAVE TO CHANGE IN ONLY ONE PLACE IN ORDER TO SWITCH ...
+//THATS IS DEPENDENCY INVERSION PRINCIPLE ...
+//another eg...
+/*
+
+   class Store {
+    products :  number;
+payment : RazorPay ;  //type is going to be the Razorpay class ...
+    constructor(){
+   this.products = 100;
+   this.payment =new RazorPay();  //here this is how we use ...
+}
+
+    }
+
+// think we are building a ecom app where absolutely payment is fundamentally required  we have some built in solution for that ...
+     //1. stripe(Global)
+     //2.  Razorpay(India)
+
+     class RazorPay{
+        constructor(){
+
+        }
+
+        payWithUs(rs :number){
+            console.log(rs);
+        }
+     }
+//now if our Razorpay works in india only and if i want to go with stripe globally ... when our app is too big ...
+
+
+
+class Stripe{
+    constructor(){
+
+    }
+
+    payWithElse(usd:number){
+        console.log(usd);
+    }
+}
+
+//now if i have to change our payment gateway ...
+ //but there is some issue in both payment wala classes
+//both having diff function name and with diff diff parameters ...
+
+
+//so what is the solution ... ? => SHOULD OUR STORE DIRECTLY DEPENDS ON THE OUTER DEPENDICIES .... no
+
+*/
+class Store {
+    constructor() {
+        this.products = 100;
+    }
+}
+// DEPENDENCY INVERSION PRINCIPLE OUR...
+class RazorPayManager {
+    constructor() {
+        this.razorpay = new RazorPay();
+    }
+    pay(rs) {
+        this.razorpay.payWithUs(rs);
+    }
+}
+class RazorPay {
+    constructor() { }
+    payWithUs(inr) {
+        console.log(inr);
+    }
+}
+//similarly creating same sort of class for stripe also ... 
+class StripePayManager {
+    constructor() {
+        this.stripepay = new StripePay();
+    }
+    pay(usd) {
+        this.stripepay.payWithst(usd);
+    }
+}
+class StripePay {
+    constructor() { }
+    payWithst(usd) {
+        console.log(usd);
+    }
+}
+// const manager = new RazorPayManager();
+// manager.pay(500); // Output: Payment of ₹500 processed with RazorPay.
+//now the manager class plays role for independency acchivement and can be used ob baasis of dependency inversion Principle ...
+// we'll make 
+class payments {
+} //and will link the paymentmanager and store together with this  this will be a abstraction layer .
--- 07.DESIGN_PATTERNS/Design-Pattern.js
+++ 07.DESIGN_PATTERNS/Design-Pattern.js
@@ -1,18 +1,19 @@
+"use strict";
 /*
-Design Patterns :- 
+Design Patterns :-
         SOLVING COMMON SOFTWARE ENGINEERING... PROBLEMS  ....
 
 
 //WHAT HAPPENS IS ... WHENVER PEOPOPLE ARE BUILDING SOME COMPLEX SOFTWARE RELATED PROBLEM THE PROBLEM ITSELF NOT BE THE SAME ...
 //LETS TAKE AN EG.. LOKENDRA IS BUILDING A MOBLIE APPLICATION ...AND UPENDRA IS BUILDING A DESKTOP APPLICATION ...
-//YOU GET SOME PROBLEMS YOU BREAK IT DOWN INTO SMALLER PROBLEMS AND TRY TO SOLVE IT IN SMALLER CHNCKS 
+//YOU GET SOME PROBLEMS YOU BREAK IT DOWN INTO SMALLER PROBLEMS AND TRY TO SOLVE IT IN SMALLER CHNCKS
 //ENGINEERS FACED THAT ...THEY ARE FACING SOME COMMON CHALLANGE WHILE BUILDING APPS FOR DIFF PLATEFORMS ...
-//TO TACKEL THIS COMMON PROBLEM THEY ARE FACING THEY BUILD SOMETHING KNOWN AS DESIGN PATTERNS ... 
+//TO TACKEL THIS COMMON PROBLEM THEY ARE FACING THEY BUILD SOMETHING KNOWN AS DESIGN PATTERNS ...
 
 
-//USING THIS DESIGN PATTERNS I CAN SOLVE SOME COMMON SOFTWARE ENGINEERING PROBLEMS 
+//USING THIS DESIGN PATTERNS I CAN SOLVE SOME COMMON SOFTWARE ENGINEERING PROBLEMS
     
-there are lot of design patterns... 
+there are lot of design patterns...
                                             ___________________________
                                                  DESIGN PATTERNS
                                             _____________________________
@@ -20,12 +21,12 @@ there are lot of design patterns...
    
                                     /          /               \              \
                                   
-                       Creational          structural        Behavioral      Architectural 
-                     design pattern       design pattern   design pattern   design pattern           
+                       Creational          structural        Behavioral      Architectural
+                     design pattern       design pattern   design pattern   design pattern
                          \                        |                |              |
                          \                       |                 |              |
                      singleton pattern            |                |               |
-                        \                        |               Observer          | 
+                        \                        |               Observer          |
                         \                        |                Pattern          |
                         \                        |                                 |
                      Factory Pattern        FacadePattern                      MVC pattern
@@ -36,97 +37,46 @@ there are lot of design patterns...
                    
           
 
-4.Architectural design : - pattern your whole structure(application) depends on   (type-mvc)        
+4.Architectural design : - pattern your whole structure(application) depends on   (type-mvc)
 
 
 */
-
-
-
-
 // 1.Creational design patterns are about how to create objects .
-
 /*
    
-i> singleton creational design pattern  -> One Instance OUT of A class. 
-                        eg. express instance 
-                            const app = express(); 
-                        eg. const sequelize = new Sequelize();  
-                        eg. const db = Database.connenct(); 
-
+i> singleton creational design pattern  -> One Instance of a class.
+                        eg. express instance
+                            const app = express();
+                        eg. const sequelize = new Sequelize();
 */
-
-
 //we want only one instance ... 
 class Database {
-    url: string;
-    //we make the constructor itself private ...
-    static Instance: Database | null = null;    //one is type and other is value . 
-    private constructor(url: string) {
+    constructor(url) {
         this.url = url;
     }
     create() {
-        console.log("ok")
+        console.log("ok");
     }
-
     //we created this static property... 
-    static connect(url: string) {
-
-        if (Database.Instance !== null) {  //for tracking purpose
+    static connect(url) {
+        if (Database.Instance !== null) { //for tracking purpose
             return Database.Instance;
         }
-
-        const db = new Database(url);  //privet keyword allows to use property within the class only . 
+        const db = new Database(url); //privet keyword allows to use property within the class only . 
         Database.Instance = db; //upgrading flag
-
         return Database.Instance;
     }
-
 }
-
-
+//we make the constructor itself private ...
+Database.Instance = null; //one is type and other is value . 
 // const db = new Database("urldfaf343ff");
 //if constructor of superclass is privet we cannot create any instance now ... 
 // we create static connect fun there which is returning instance for us
-
 // how can we connect the static property 
-
 const db = Database.connect("urlxdsfsadf/dbname"); //have you ever saw the same code somewhere... haaha ... 
 //   Database.connect(" ")
 //again it will create another instance dont you think
 //so whats the solution => keep counter track ... 
-
 //after some modification line 62/73/78 
-
 const db2 = Database.connect("dfsaf");
-console.log(db, db2)  //both will have same instance ...ie we got our goal ..
-
-
-//SINGLE TON PATTERN IS VERY MUCH USED IN INDUSTRY STANDARD... 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(db, db2); //both will have same instance ...ie we got our goal ..
